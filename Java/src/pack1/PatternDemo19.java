ec001ea5a3ebebca3096b688b96a5d619c74facc
Sanket Bhavar <bhavarsanket2710@gmail.com>
2022-01-05 21:23:24 +0530

    Star Pattern

---
+++ Java/src/pack1/PatternDemo19.java
@@ -0,0 +1,25 @@
+package pack1;
+
+public class PatternDemo19 {
+	public static void main(String[] args) {
+		int i,j,k;
+		for(i=1;i<=5;i++)
+		{
+			for(k=1;k<=i;k++)
+			{
+				System.out.print(" ");
+			}
+			for(j=1;j<=2*(5-i+1)-1;j++)
+			{
+				if(i==1||j==1||j==2*(5-i+1)-1)
+				{
+					System.out.print("*");
+				}
+				else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+	}
+}
