a5fed6bb7c7142af1e0ff737ddeab726f623bce0
Sanket Bhavar <bhavarsanket2710@gmail.com>
2022-01-10 19:00:55 +0530

    2D Array

---
+++ Java/src/pack1/TwoDArray.java
@@ -0,0 +1,29 @@
+package pack1;
+
+import java.util.Scanner;
+
+public class TwoDArray {
+	public static void main(String[] args) {
+		int a[][];
+		a=new int[3][4];
+		Scanner sc=new Scanner(System.in);
+		int i,j;
+		for(i=0;i<=a.length-1;i++)
+		{
+			for(j=0;j<=a[i].length-1;j++)
+			{
+				System.out.println("Enter an element");
+				a[i][j]=sc.nextInt();
+			}
+		}
+		for(i=0;i<=a.length-1;i++)
+		{
+			for(j=0;j<=a[i].length-1;j++)
+			{
+				System.out.print(a[i][j]+" ");
+			}
+			System.out.println();
+		}
+		sc.close();
+	}
+}
