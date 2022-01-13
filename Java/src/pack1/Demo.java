c8d84cb1c7f76084e8b71d51d1e06ed328aa9795
Sanket Bhavar <bhavarsanket2710@gmail.com>
2022-01-13 16:02:08 +0530

    Count Function

---
+++ Java/src/pack1/Demo.java
@@ -0,0 +1,33 @@
+package pack1;
+class Demo1
+{
+	int a,b;
+	static int count;
+	{
+		count++;
+	}
+	Demo1(){
+		
+	}
+	Demo1(int x)
+	{
+		a=x;
+		b=10;
+	}
+	Demo1(int x, int y)
+	{
+		a=x;
+		b=y;
+	}
+}
+class Demo {
+	public static void main(String[] args) {
+	Demo1 d1,d2,d3,d4,d5;
+	d1=new Demo1();
+	d2=new Demo1();
+	d3=new Demo1();
+	d4=new Demo1(10);
+	d5=new Demo1(50,60);
+	System.out.println("Total no. of objects: "+Demo1.count);
+}
+}
