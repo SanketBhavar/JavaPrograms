e50359478333c560d70148022f5a871a3760be67
Sanket Bhavar <bhavarsanket2710@gmail.com>
2022-01-18 21:34:02 +0530

    No. of Occurrences in a string

---
+++ Java/src/pack1/CharCount.java
@@ -0,0 +1,28 @@
+package pack1;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class CharCount {
+	public static void main(String[] args) throws IOException {
+		String s1;
+		char ch;
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter a String");
+		s1=scan.next();
+		System.out.println("Enter a Character");
+		ch=(char)System.in.read();
+		int m,i;
+		m=s1.length();
+		int count=0;
+		for(i=0;i<m;i++)
+		{
+			if(s1.charAt(i)==ch)
+			{
+				count++;
+			}
+		}
+		System.out.println(count);
+		scan.close();
+	}
+}
