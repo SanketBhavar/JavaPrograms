cb1112e97a643d55845906a93731ea8c20966d4c
Sanket Bhavar <bhavarsanket2710@gmail.com>
2022-02-17 20:57:06 +0530

    Building Custom Exception

---
+++ Java/src/pack1/ExceptionDemo10.java
@@ -0,0 +1,33 @@
+package pack1;
+
+import java.util.Scanner;
+
+public class ExceptionDemo10 {
+	public static void main(String[] args) {
+		int a,b,c;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter value of a");
+		a=sc.nextInt();
+		System.out.println("Enter value of b");
+		b=sc.nextInt();
+		try {
+			if(a>b) {
+				c=a-b;
+				System.out.println(c);
+			}
+			else {
+				throw new ALessThanBException();
+			}
+		}
+		catch(ALessThanBException e) {
+			System.out.println("sub not possible...");
+		}
+		System.out.println("Exiting main normally");
+	}
+}
+class ALessThanBException extends Exception
+{
+	ALessThanBException(){
+		System.out.println("A is less than B");
+	}
+}
