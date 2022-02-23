49461fa3ae8c8ad2343096332c28f8b5f270f208
Sanket Bhavar <bhavarsanket2710@gmail.com>
2022-02-23 20:44:56 +0530

    Student Marks

---
+++ Java/src/pack1/ArrayExample.java
@@ -0,0 +1,21 @@
+package pack1;
+
+import java.util.Scanner;
+
+public class ArrayExample {
+	public static void main(String[] args) {
+		int marks[];
+	    marks= new int[5];
+	    Scanner input= new Scanner(System.in);
+	    marks[0]=input.nextInt();
+	    marks[1]=input.nextInt();
+	    marks[2]=input.nextInt();
+	    marks[3]=input.nextInt();
+	    marks[4]=input.nextInt();
+
+	    int rollNumber;
+	    rollNumber=input.nextInt();
+	    System.out.println("The student with roll number "+rollNumber+" has scored "+marks[rollNumber]+" marks");
+	    input.close();
+	}
+}
