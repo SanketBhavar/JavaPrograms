8cf2d3bd3443a6d932b1c1e0e15568a8a772cf64
Sanket Bhavar <bhavarsanket2710@gmail.com>
2022-02-25 21:42:37 +0530

    Positive Number or Not

---
+++ Java/src/pack1/PositiveNo.java
@@ -0,0 +1,18 @@
+package pack1;
+
+import java.util.Scanner;
+
+public class PositiveNo {
+	 public static void main(String[] args) {
+		 int n;
+		 Scanner input= new Scanner(System.in);
+		 n=input.nextInt();
+		 if (n>0){
+			 System.out.println("The number entered is positive");
+		 }
+		 else{
+		     System.out.println("The number entered is negative");
+		 }
+		 input.close();
+	 }
+}
