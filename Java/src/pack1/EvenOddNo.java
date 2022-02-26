c1dffb92d8a202bf62fe327e5f39f99761325e41
Sanket Bhavar <bhavarsanket2710@gmail.com>
2022-02-26 20:52:47 +0530

    Even Odd No

---
+++ Java/src/pack1/EvenOddNo.java
@@ -0,0 +1,18 @@
+package pack1;
+
+import java.util.Scanner;
+
+class EvenOddNo{
+	public static void main(String[] args) {
+		int n;
+		Scanner input= new Scanner(System.in);
+		n=input.nextInt();
+		if (n%2==0){
+			System.out.println("The number entered is even");
+		}
+		else{
+			System.out.println("The number entered is odd");
+		}
+		input.close();
+   }
+}
