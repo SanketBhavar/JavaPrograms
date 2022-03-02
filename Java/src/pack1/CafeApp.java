aeabe471b1931372597fb5aca9c773e62fd1febe
Sanket Bhavar <bhavarsanket2710@gmail.com>
2022-03-02 20:07:46 +0530

    Switch Case Example-1

---
+++ Java/src/pack1/CafeApp.java
@@ -0,0 +1,30 @@
+package pack1;
+
+import java.util.Scanner;
+
+public class CafeApp {
+	public static void main(String args[]){
+		 int code;
+		 Scanner input = new Scanner(System.in);
+		 System.out.println("Enter your Choice:");
+		 code = input.nextInt();
+		 
+		switch(code) {
+		case 1 :
+		      System.out.println("Preparing Cappuccino");
+		      break;
+		case 2:
+		      System.out.println("Preparing Espresso");
+		      break;
+		case 3:
+		      System.out.println("Preparing Latte");
+		      break;
+		case 4:
+		      System.out.println("Preparing Milk");
+		      break;
+		default:
+		     System.out.println("Drink not Available");
+		}
+	input.close();
+	}
+}
