4ba312fcc99fc8b5c77a98df1b79b50709863001
Sanket Bhavar <bhavarsanket2710@gmail.com>
2022-03-05 21:39:16 +0530

    Voting Example

---
+++ Java/src/pack1/VotingApp.java
@@ -0,0 +1,30 @@
+package pack1;
+
+import java.util.Scanner;
+
+public class VotingApp {
+	public static void main(String[] args) {
+		int age, vote;
+	    Scanner input= new Scanner(System.in);
+	    age=input.nextInt();
+	    vote=input.nextInt();
+	    if (age>=18){
+	       if(vote==1)
+	       {
+	           System.out.println("You have voted for Ram");
+	       }
+	       else if(vote==2)
+	       {
+	           System.out.println("You have voted for Shyam");
+	       }
+	       else
+	       {
+	           System.out.println("You have voted for Ghanshyam");
+	       }
+	     }
+	     else{
+	        System.out.println("You are not eligible");
+	     }
+	    input.close();
+	}
+}
