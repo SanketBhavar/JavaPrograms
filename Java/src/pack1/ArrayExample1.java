2f921571c8417b0a11872b17f6785b3d073e46b3
Sanket Bhavar <bhavarsanket2710@gmail.com>
2022-03-15 22:03:01 +0530

    Math.round Method

---
+++ Java/src/pack1/ArrayExample1.java
@@ -0,0 +1,14 @@
+package pack1;
+
+public class ArrayExample1 {
+	public static void main(String[] args) { 
+		double[] array = {10.32,20.36,30.50,40.51,50.49,60.43,71.71};
+
+		for(int i = 0; i <  array.length; i++) {
+			array[i]=Math.round(array[i]);
+		}
+		for(int i = 0; i <  array.length; i++) {
+			System.out.print(array[i] + " ");
+		}
+	}
+}
