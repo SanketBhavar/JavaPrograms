4c88ecd0b09a4fb542ac0885952adfa646fdbd11
Sanket Bhavar <bhavarsanket2710@gmail.com>
2022-03-22 21:28:48 +0530

    Searching Example

---
+++ Java/src/pack1/Searching.java
@@ -0,0 +1,28 @@
+package pack1;
+import java.util.Scanner;
+
+public class Searching {
+   public static void main(String args[]) {
+       int count;
+       int i;
+       
+       Scanner input = new Scanner(System.in);
+       count = input.nextInt();
+       int array[] = new int[count];
+       
+       for (i = 0; i < count; i++) {
+           array[i] = input.nextInt();
+       }
+      
+       int key = input.nextInt();
+       input.close();
+       int result=0;
+       for (i = 0; i < count; i++) {
+           if (array[i] == key) {
+               result = (count-1)-i;
+           }
+       }
+       if (result>=0) System.out.println(result);
+
+   }
+}
