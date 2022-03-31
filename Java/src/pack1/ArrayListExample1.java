2ac72ec6780be47512ff059cc445ffb4b899ac8d
Sanket Bhavar <bhavarsanket2710@gmail.com>
2022-03-31 18:26:08 +0530

    Add Float Element in ArrayList

---
+++ Java/src/pack1/ArrayListExample1.java
@@ -0,0 +1,21 @@
+package pack1;
+
+import java.util.ArrayList;
+
+public class ArrayListExample1 {
+	public static void main(String[] args) {
+		ArrayList<Float> random = new ArrayList<Float>();
+		random.add(2f);
+		random.add(4f);
+		random.add(5f);
+		random.add(10f);
+		random.add(99.9f);
+
+		printList(random);
+	}
+	public static void printList(ArrayList<Float> arr) {
+		for(Float a : arr) {
+			System.out.println(a);
+		}
+	}
+}
