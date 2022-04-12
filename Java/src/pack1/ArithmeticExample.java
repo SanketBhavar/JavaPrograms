3c77e6c4c44ad325ca3f4371ff9c6ef6be08d91d
Sanket Bhavar <bhavarsanket2710@gmail.com>
2022-04-12 18:29:17 +0530

    Arithmetic Operation

---
+++ Java/src/pack1/ArithmeticExample.java
@@ -0,0 +1,20 @@
+package pack1;
+
+import java.util.Scanner;
+
+public class ArithmeticExample {
+	public static void main( String[] args) 
+	{
+		int a;
+	    int b;
+	    int c;
+	    System.out.println("Enter values of A, B, C=>");
+	    
+	    Scanner sc = new Scanner(System.in);
+	    a= sc.nextInt();
+	    b= sc.nextInt();
+	    c= sc.nextInt();
+	    System.out.println(a*b+c);
+	    sc.close();
+	}
+}
