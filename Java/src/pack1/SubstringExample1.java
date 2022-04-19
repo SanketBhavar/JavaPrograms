20a4e94b9b43c0c3a05520d12861b6295dc4844e
Sanket Bhavar <bhavarsanket2710@gmail.com>
2022-04-19 18:03:43 +0530

    Repeat Substring Many Times

---
+++ Java/src/pack1/SubstringExample1.java
@@ -0,0 +1,15 @@
+package pack1;
+
+public class SubstringExample1 {
+	public String extraEnd(String str)
+	{
+		String s1=str.substring(str.length()-2, str.length());
+		return s1+s1+s1;
+	}
+	public static void main(String[] args) 
+	{
+		SubstringExample1 s= new SubstringExample1();
+		String res=s.extraEnd("Hi");
+		System.out.println(res);
+	}
+}
