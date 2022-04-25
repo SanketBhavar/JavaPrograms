1a621dc138f847b470b9d3344348382154601ae8
Sanket Bhavar <bhavarsanket2710@gmail.com>
2022-04-25 17:04:44 +0530

    Count Hi in a String

---
+++ Java/src/pack1/CountHiInString.java
@@ -0,0 +1,21 @@
+package pack1;
+
+public class CountHiInString {
+	public int countHi(String str) 
+	{
+		int count = 0;
+		for(int i=0; i<str.length()-1; i++)
+		{
+			if(str.charAt(i) == 'h' && str.charAt(i+1) == 'i')
+			count++;
+		}
+		return count;
+	}
+
+	public static void main(String[] args) 
+	{
+		CountHiInString ch= new CountHiInString();
+		int res=ch.countHi("hi hello hi");
+		System.out.println(res);
+	}
+}
