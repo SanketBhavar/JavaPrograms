6ab9a5b04b706d4a9dc948f471725786f6a13704
Sanket Bhavar <88875216+SanketBhavar@users.noreply.github.com>
2022-07-15 21:43:46 +0530

    Update AlcoholExample.java

--- Java/src/pack1/AlcoholExample.java
+++ Java/src/pack1/AlcoholExample.java
@@ -4,16 +4,16 @@ import java.util.Scanner;
 
 public class AlcoholExample {
 	public static void main(String[] args) {
-		int age;
+	    int age;
 	    Scanner input= new Scanner(System.in);
 	    age=input.nextInt();
 	    if (age>=21)
 	    {
-	        System.out.println("You are eligible to consume alcohol");
+	        System.out.println("***You are Eligible to Consume Alcohol***");
 	    }
 	    else
 	    {
-	        System.out.println("You are not eligible to consume alcohol. Go home kid!");
+	        System.out.println("You are Not Eligible to Consume Alcohol. Go Home Kid!!!");
 	    }
 	    input.close();
 	}
