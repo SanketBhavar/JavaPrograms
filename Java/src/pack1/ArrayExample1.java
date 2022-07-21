bb7381f2d3db006ed60fc3302b2406d83ef9f91d
Sanket Bhavar <88875216+SanketBhavar@users.noreply.github.com>
2022-07-21 20:43:13 +0530

    Math.round() function

--- Java/src/pack1/ArrayExample1.java
+++ Java/src/pack1/ArrayExample1.java
@@ -1,16 +1,17 @@
 package pack1;
 
-
+import java.lang.Math;
 
 public class ArrayExample1 {
 	public static void main(String[] args) { 
 		double[] array = {10.32,20.36,30.50,40.51,50.49,60.43,71.71};
 
-		for(int i = 0; i <  array.length; i++) {
+		for(int i = 0; i < array.length; i++) {
 			array[i]=Math.round(array[i]);
 		}
-		for(int i = 0; i <  array.length; i++) {
-			System.out.print("array[i] + " ");
+		System.out.print("After Applying Math.round() function values are");
+		for(int i = 0; i < array.length; i++) {
+			System.out.print(array[i] + " ");
 		}
 	}
 }
