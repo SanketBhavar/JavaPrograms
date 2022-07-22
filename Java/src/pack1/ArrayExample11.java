f5447edf1677d4fe837d2d07622ae98682757e93
Sanket Bhavar <88875216+SanketBhavar@users.noreply.github.com>
2022-07-22 19:06:11 +0530

    Array Example

--- Java/src/pack1/ArrayExample11.java
+++ Java/src/pack1/ArrayExample11.java
@@ -9,18 +9,17 @@ public class ArrayExample11 {
 		int[] array1 = new int[n1];
 		for (int i = 0; i < n1; i++) {
 			array1[i] = s.nextInt();
-		}
-		
+		}		
 		int n2 = s.nextInt();
 		int[] array2 = new int[n2];
 		for (int i = 0; i < n2; i++) {
 			array2[i] = s.nextInt();
 		}
-		
 		int temp1=0;
 		int temp2=0;
 		for(int i=0; i < array1.length-1;i++) 
-			if (array1[i] > array1[i+1]){ 
+			if (array1[i] > array1[i+1])
+			{ 
 				temp1=array1[i];
 				temp2=array1[i+2];
 			}   
@@ -30,6 +29,11 @@ public class ArrayExample11 {
 				if(array2[j]>temp3)
 					temp3 = array2[j];
 
-				s.close();
+		if(temp3==-1)
+			System.out.println("Not Possible \n");
+		else
+			System.out.println(temp3);
+			
+		s.close();
 	}
 }
