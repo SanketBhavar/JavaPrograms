5438bc539cfd92cda6b61a0493698945f312d801
Sanket Bhavar <88875216+SanketBhavar@users.noreply.github.com>
2022-07-23 21:19:51 +0530

    Update ArrayExample2.java

--- Java/src/pack1/ArrayExample2.java
+++ Java/src/pack1/ArrayExample2.java
@@ -4,8 +4,9 @@ import java.util.Arrays;
 
 public class ArrayExample2 {
 	public static void main(String[] args) {
-		int[] array = {29,34,12,45,56,02,43, 19};
+	    int[] array = {29,34,12,45,56,02,43, 19};
 	    Arrays.sort(array);
+		System.out.print("");
 	    for(int i=0; i<array.length; i++) {
 	    	System.out.print(array[i] + " ");
 	    }
