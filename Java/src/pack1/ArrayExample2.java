8bb3c02726c371b0a8e39738c44f80f6b8db068b
Sanket Bhavar <88875216+SanketBhavar@users.noreply.github.com>
2022-07-23 21:21:45 +0530

    Arrays.sort method

--- Java/src/pack1/ArrayExample2.java
+++ Java/src/pack1/ArrayExample2.java
@@ -4,9 +4,9 @@ import java.util.Arrays;
 
 public class ArrayExample2 {
 	public static void main(String[] args) {
-	    int[] array = {29,34,12,45,56,02,43, 19};
+	    int[] array = {29,34,12,45,56,02,43,19};
 	    Arrays.sort(array);
-		System.out.print("");
+	    System.out.print("Array Element Sorting in Ascending Order");
 	    for(int i=0; i<array.length; i++) {
 	    	System.out.print(array[i] + " ");
 	    }
