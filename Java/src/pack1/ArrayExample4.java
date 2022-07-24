f7f3b19ae3b8109943b92a099b0e016285389410
Sanket Bhavar <88875216+SanketBhavar@users.noreply.github.com>
2022-07-24 21:09:58 +0530

    Math.sqrt() function

--- Java/src/pack1/ArrayExample4.java
+++ Java/src/pack1/ArrayExample4.java
@@ -1,5 +1,7 @@
 package pack1;
 
+import java.lang.Math;
+
 public class ArrayExample4 {
 	public static void main(String[] args) { 
 		double[] array = {10.32,20.36,30.50,40.51,50.49,60.43,71.71};
@@ -7,6 +9,7 @@ public class ArrayExample4 {
 		for(int i = 0; i <  array.length; i++) {
 			array[i]=Math.sqrt(array[i]);
 		}
+		System.out.println("Math.sqrt() Function");
 		for(int i = 0; i <  array.length; i++) {
 			System.out.print(array[i] + " ");
 		}
