c6d155811a1d57b6c541bed80ee291367ca5cedc
Sanket Bhavar <88875216+SanketBhavar@users.noreply.github.com>
2022-07-24 21:06:34 +0530

    Math.ceil() Function

--- Java/src/pack1/ArrayExample3.java
+++ Java/src/pack1/ArrayExample3.java
@@ -1,5 +1,7 @@
 package pack1;
 
+import java.lang.Math;
+
 public class ArrayExample3 {
 	public static void main(String[] args) { 
 		double[] array = {10.32,20.36,30.50,40.51,50.49,60.43,71.71};
@@ -7,10 +9,9 @@ public class ArrayExample3 {
 		for(int i = 0; i <  array.length; i++) {
 			array[i]=Math.ceil(array[i]);
 		}
+		System.out.println("Math.ceil Function")
 		for(int i = 0; i <  array.length; i++) {
 			System.out.print(array[i] + " ");
-		}
-		
-	}
-	
+		}	
+	}	
 }
