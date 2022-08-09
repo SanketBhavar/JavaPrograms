4117ed93672ef52aede99cf2219eee7425df1458
Sanket Bhavar <88875216+SanketBhavar@users.noreply.github.com>
2022-08-09 17:22:44 +0530

    compareTo() Function

--- Java/src/pack1/CompareToDemo.java
+++ Java/src/pack1/CompareToDemo.java
@@ -1,5 +1,7 @@
 package pack1;
 
+import java.util.Scanner;
+
 public class CompareToDemo {
 	public static void main(String[] args) {
 		String s1,s2;
@@ -10,12 +12,13 @@ public class CompareToDemo {
 		s2=scan.next();
 		
 		int res;
-		res=s1.compareTo(s2);
+		res=s1.compareTo(s2); 		//CompareTo() function for Comparing String1 and String2
 		if(res==0) {
 			System.out.println("s1 is equal to s2");
 		}
 		else {
 			System.out.println("s1 is not equal to s2");
-		}		
+		}
+	scan.close();
 	}
 }
