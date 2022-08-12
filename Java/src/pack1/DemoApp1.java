8a394b40a75981fd4d276c1fd98269fd564812a4
Sanket Bhavar <88875216+SanketBhavar@users.noreply.github.com>
2022-08-12 18:48:40 +0530

    Interface in Java-1

--- Java/src/pack1/DemoApp1.java
+++ Java/src/pack1/DemoApp1.java
@@ -1,1 +1,33 @@
 package pack1;
+
+interface Compute{
+  void add();
+	void sub();
+}
+
+//Interface in Java
+class Addition1 implements Compute{
+	public void add() {
+		int a,b,c;
+		a=10;
+		b=20;
+		c=a+b;
+		System.out.println(c);
+	}
+	public void sub() {
+		int a,b,c;
+		a=10;
+		b=20;
+		c=a-b;
+		System.out.println(c);
+	}
+}
+public class DemoApp1 {
+	public static void main(String[] args) {
+		Addition1 a = new Addition1();
+		Compute ptr;
+		ptr=a;
+		ptr.add();
+		ptr.sub();
+	}
+}
