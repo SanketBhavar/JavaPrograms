9dc584db945ba6673ce5c922c5cfba5ee58a22cd
Sanket Bhavar <88875216+SanketBhavar@users.noreply.github.com>
2022-08-16 16:09:20 +0530

    Check Number is Duck or Not

--- Java/src/pack1/DuckNo.java
+++ Java/src/pack1/DuckNo.java
@@ -1,1 +1,22 @@
 package pack1;
+
+import java.util.Scanner;
+
+public class DuckNo {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		String num=sc.next();
+		if(num.charAt(0)=='0') {
+			System.out.println("Given Number is not a Duck Number");
+		}
+		else {
+			if(num.contains("0")) {
+				System.out.println("Given Number is Duck Number");
+			}
+			else {
+				System.out.println("Given Number is not a Duck Number");
+			}
+		}
+		sc.close();
+	}
+}
