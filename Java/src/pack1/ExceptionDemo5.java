a1969c4bffe51426a1cb1022e01303cbffb805fb
Sanket Bhavar <88875216+SanketBhavar@users.noreply.github.com>
2022-08-25 21:31:27 +0530

    Without Finally Block

--- Java/src/pack1/ExceptionDemo5.java
+++ Java/src/pack1/ExceptionDemo5.java
@@ -1,1 +1,40 @@
 package pack1;
+public class ExceptionDemo5 {
+	public static void main(String[] args) {
+		System.out.println("Main Connected to DataBase");
+		Demo5 d5=new Demo5();
+		try {
+			d5.fun1();
+		}
+		catch(Exception e) {
+			System.out.println("Exception Caught in main");
+		}
+	}
+}
+
+class Demo5{
+	void fun1() {
+		System.out.println("Fun1 Connected to DataBase");
+		try {
+			fun2();
+		}
+		catch(Exception e) {
+			System.out.println("Exception Caught in Fun1");
+			throw e;
+		}
+	}
+	void fun2() {
+		System.out.println("Fun2 Connected to DataBase");
+		try {
+			int a,b,c;
+			a=10;
+			b=0;
+			c=a/b;
+			System.out.println(c);
+		}
+		catch(Exception e) {
+			System.out.println("Exception Caught in Fun2");
+			throw e;
+		}
+	}
+}
