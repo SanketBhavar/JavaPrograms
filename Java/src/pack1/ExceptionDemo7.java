af28d1e8b962f4acd09b2bf07601b7886dc45833
Sanket Bhavar <88875216+SanketBhavar@users.noreply.github.com>
2022-08-29 20:33:33 +0530

    Use of System.exit(0)

--- Java/src/pack1/ExceptionDemo7.java
+++ Java/src/pack1/ExceptionDemo7.java
@@ -1,1 +1,22 @@
 package pack1;
+
+public class ExceptionDemo7 {
+	public static void main(String[] args) {
+		int a,b;
+		a=10;
+		b=5;
+		int c;
+		try {
+			c=a/b;
+			System.out.println(c);
+			System.out.println("Try Executed");
+			System.exit(0);                     //use of system.exit(0) - it exit from execution
+		}
+		catch(Exception e) {
+			System.out.println("Problem in Division");
+		}
+		finally {
+			System.out.println("Finally Executed");
+		}
+	}
+}
