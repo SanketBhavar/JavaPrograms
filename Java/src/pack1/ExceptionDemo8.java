de60f710b87daf8bace13a2a0536ef52052ef566
Sanket Bhavar <88875216+SanketBhavar@users.noreply.github.com>
2022-08-30 20:19:11 +0530

    getMessage() Method

--- Java/src/pack1/ExceptionDemo8.java
+++ Java/src/pack1/ExceptionDemo8.java
@@ -1,1 +1,18 @@
 package pack1;
+
+public class ExceptionDemo8 {
+	public static void main(String[] args) {
+		int a,b,c;
+		a=10;
+		b=0;
+		
+		try {
+			c=a/b;
+			System.out.println(c);
+		}
+		catch(Exception e) {
+			System.out.println(e.getMessage());
+		}
+		System.out.println("Exiting Main Normally");
+	}
+}
