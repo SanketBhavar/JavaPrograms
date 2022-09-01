acf1e3b8734f6372015b2ccbc55f0fb9cbb7aae1
Sanket Bhavar <88875216+SanketBhavar@users.noreply.github.com>
2022-09-01 21:05:08 +0530

    Update ExceptionDemo9.java

--- Java/src/pack1/ExceptionDemo9.java
+++ Java/src/pack1/ExceptionDemo9.java
@@ -1,28 +1,2 @@
 package pack1;
 
-import java.util.Scanner;
-
-public class ExceptionDemo9 {
-	public static void main(String[] args) {
-		int a,b,c;
-		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter a value of a");
-		a=sc.nextInt();
-		System.out.println("Enter a value of b");
-		b=sc.nextInt();
-		try {
-			if(a>b) {
-				c=a-b;
-				System.out.println(c);
-			}
-			else {
-				Exception e=new Exception();   //build custom exception
-				throw e;
-			}
-		}
-		catch(Exception e) {
-			System.out.println("sub not possible");
-		}
-		System.out.println("Exiting main normally");
-	}
-}
