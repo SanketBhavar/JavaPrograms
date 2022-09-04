2acc072d8fbb47db694797d589ffe26f26fef04e
Sanket Bhavar <88875216+SanketBhavar@users.noreply.github.com>
2022-09-04 21:32:03 +0530

    Update FunctionExample.java

--- Java/src/pack1/FunctionExample.java
+++ Java/src/pack1/FunctionExample.java
@@ -1,24 +1,2 @@
 package pack1;
 
-import java.util.Scanner;
-
-public class FunctionExample {
-	public static int sum(int n){
-	    if (n!= 0)
-	            return n + sum(n - 1);
-	        else
-	            return n;
-	  }
-	   public static void main(String args[]){
-	       Scanner sc = new Scanner(System.in);
-	       int n = sc.nextInt();
-	       if (n>0){
-	           int value = FunctionExample.sum(n);
-	           System.out.print(value);
-	       }
-	       else{
-	            System.out.println("Enter a natural number");
-	       }
-	       sc.close();
-	}
-}
