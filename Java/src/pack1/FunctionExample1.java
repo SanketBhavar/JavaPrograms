d6c9c40222871036bb2138a2d486e41ed8051814
Sanket Bhavar <88875216+SanketBhavar@users.noreply.github.com>
2022-09-05 20:47:36 +0530

    Update FunctionExample1.java

--- Java/src/pack1/FunctionExample1.java
+++ Java/src/pack1/FunctionExample1.java
@@ -2,19 +2,8 @@ package pack1;
 
 import java.util.Scanner;
 
-public class FunctionExample1 {
-	public static int powerfunc(int base, int power) {
-        if(power==0)
-         return 1;
-     else 
-         return (base*powerfunc(base,power-1)); 
-    }
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        System.out.printf("%d", powerfunc(a, b));
-        sc.close();
-    }
 
-}
+
+
+
+
