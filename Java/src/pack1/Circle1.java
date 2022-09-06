66b301f15161bbcafe5a82e4f639362e5c5a7db8
Sanket Bhavar <88875216+SanketBhavar@users.noreply.github.com>
2022-09-06 09:05:33 +0530

    Area of Circle

--- Java/src/pack1/Circle1.java
+++ Java/src/pack1/Circle1.java
@@ -1,3 +1,11 @@
 package pack1;
 
-
+public class Circle1{
+    public static float radius = 10.0f;
+    public static void main(String[] a){
+        System.out.println(area(radius));
+    }
+public static float area(float radius){     //Calculating the Area of Circle
+        return 3.141f * radius * radius;
+    }
+}
