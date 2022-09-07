7934feb37c1c2f15a2da5ac1d561bfc22d22d839
Sanket Bhavar <88875216+SanketBhavar@users.noreply.github.com>
2022-09-07 15:37:00 +0530

    Update InputDemo.java

--- Java/src/pack1/InputDemo.java
+++ Java/src/pack1/InputDemo.java
@@ -1,17 +1,2 @@
 package pack1;
 
-import java.util.Scanner;
-
-public class InputDemo {
-	public static void main(String[] args) {
-		int a,b,c;
-		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter an Element");
-		a=sc.nextInt();
-		System.out.println("Enter an Element");
-		b=sc.nextInt();
-		c=a+b;
-		System.out.println("Sum of two numbers: "+c);
-		sc.close();
-	}
-}
