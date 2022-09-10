44e6e70c520b50a13a59d507f4da4a37f8f2ed7b
Sanket Bhavar <88875216+SanketBhavar@users.noreply.github.com>
2022-09-10 18:55:58 +0530

    Update MobileApp.java

--- Java/src/pack1/MobileApp.java
+++ Java/src/pack1/MobileApp.java
@@ -1,46 +1,4 @@
 package pack1;
 
-class Os{
-	Os(){
-		System.out.println("OS is Installed");
-	}
-	void checkOs() {
-		System.out.println("OS is still Executing");
-	}
-}
 
-class Charger{
-	String name;
-	Charger(String s){
-		name=s;
-		System.out.println("Charger Created");
-	}
-	void getCharger() {
-		System.out.println("Charger can be used for charging");
-	}
-}
 
-class Mobile {
-	Os o=new Os();
-	Mobile(){
-		System.out.println("Mobile created with os installed");
-	}
-	void hasA(Charger c) {
-		System.out.println("Charger accquired");
-		System.out.println("Charger used for charging");
-	}
-}
-public class MobileApp {
-	public static void main(String[] args) {
-		Mobile m;
-		m=new Mobile();
-		Charger c;
-		c=new Charger("Iphone");
-		m.hasA(c);
-		m.o.checkOs();
-		c.getCharger();
-		m=null;
-	//	m.o.checkOs();
-		c.getCharger();
-	}
-}
