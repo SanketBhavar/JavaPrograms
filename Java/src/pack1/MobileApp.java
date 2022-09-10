645035410438b131f53296e44deee3fec64ad93d
Sanket Bhavar <88875216+SanketBhavar@users.noreply.github.com>
2022-09-10 18:59:01 +0530

    Delegation Model-1

--- Java/src/pack1/MobileApp.java
+++ Java/src/pack1/MobileApp.java
@@ -5,9 +5,10 @@ class Os{
 		System.out.println("OS is Installed");
 	}
 	void checkOs() {
-		System.out.println("OS is still Executing");
+		System.out.println("OS is Still Executing");
 	}
 }
+
 class Charger{
 	String name;
 	Charger(String s){
@@ -15,19 +16,21 @@ class Charger{
 		System.out.println("Charger Created");
 	}
 	void getCharger() {
-		System.out.println("Charger can be used for charging");
+		System.out.println("Charger Can Be Used For Charging");
 	}
 }
+
 class Mobile {
 	Os o=new Os();
 	Mobile(){
-		System.out.println("Mobile created with os installed");
+		System.out.println("Mobile Created With OS Installed");
 	}
 	void hasA(Charger c) {
-		System.out.println("Charger accquired");
-		System.out.println("Charger used for charging");
+		System.out.println("Charger Accquired");
+		System.out.println("Charger Used For Charging");
 	}
 }
+
 public class MobileApp {
 	public static void main(String[] args) {
 		Mobile m;
