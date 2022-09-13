7bf9648dbb990d8d563d077ce95d102b047b3c35
Sanket Bhavar <88875216+SanketBhavar@users.noreply.github.com>
2022-09-13 19:45:18 +0530

    Increment and Decrement Operators

--- Java/src/pack1/OperatorsExample.java
+++ Java/src/pack1/OperatorsExample.java
@@ -4,19 +4,19 @@ public class OperatorsExample {
 	int a=10;
 	void display()
     {
-		System.out.println(a++);
-        a++;
-        ++a;
-        System.out.println(++a);
-        System.out.println(a);
-        System.out.println(a++);
+	System.out.println(a++);   	//10
+        a++;			    	
+        ++a;			   	
+        System.out.println(++a);	//14	
+        System.out.println(a);		//14
+        System.out.println(a++);	//14
         a--;
-        System.out.println(a);
-        System.out.println(--a);
+        System.out.println(a);		//14
+        System.out.println(--a);	//13
         a++;
-        System.out.println(a++);
-        System.out.println(a--);
-        System.out.println(a);
+        System.out.println(a++);	//14
+        System.out.println(a--);	//15
+        System.out.println(a);		//14
     }
 	public static void main(String []args)
 	{
