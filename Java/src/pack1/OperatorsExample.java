26274a8416ad054991c999f5b25b34b2cdc8979c
Sanket Bhavar <88875216+SanketBhavar@users.noreply.github.com>
2022-09-13 19:41:32 +0530

    Update OperatorsExample.java

--- Java/src/pack1/OperatorsExample.java
+++ Java/src/pack1/OperatorsExample.java
@@ -1,1 +1,25 @@
 package pack1;
+
+public class OperatorsExample {
+	int a=10;
+	void display()
+    {
+		System.out.println(a++);
+        a++;
+        ++a;
+        System.out.println(++a);
+        System.out.println(a);
+        System.out.println(a++);
+        a--;
+        System.out.println(a);
+        System.out.println(--a);
+        a++;
+        System.out.println(a++);
+        System.out.println(a--);
+        System.out.println(a);
+    }
+	public static void main(String []args)
+	{
+		new OperatorsExample().display();
+	}
+}
