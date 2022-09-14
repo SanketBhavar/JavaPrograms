766869779181b732421af9c6ae4f6ed3b85b1d0f
Sanket Bhavar <88875216+SanketBhavar@users.noreply.github.com>
2022-09-14 20:07:01 +0530

    Increasing or Decreasing Order of Numbers

--- Java/src/pack1/OrderOfNumber.java
+++ Java/src/pack1/OrderOfNumber.java
@@ -1,2 +1,24 @@
 package pack1;
+import java.util.Scanner;
 
+public class OrderOfNumber {
+	 public static void main(String[] args) {
+
+	       int n1, n2, n3;
+	       Scanner input = new Scanner(System.in);
+	       n1 = input.nextInt();
+	       n2 = input.nextInt();
+	       n3 = input.nextInt();
+
+	       if (n1<n2 && n2<n3){
+	           System.out.println("The numbers entered are in increasing order");
+	       }
+	       else if (n1>n2 && n2>n3){
+	           System.out.println("The numbers entered are in decreasing order");
+	       }
+	       else {
+	           System.out.println("The number are in neither in increasing order nor in decreasing order");
+	       }
+	 input.close(); 
+	 }
+}
