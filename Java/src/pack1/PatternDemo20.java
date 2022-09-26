c5740db470b2e68b5ee8e8e42251f9948dae80d6
Sanket Bhavar <88875216+SanketBhavar@users.noreply.github.com>
2022-09-26 16:26:56 +0530

    Star Pattern

--- Java/src/pack1/PatternDemo20.java
+++ Java/src/pack1/PatternDemo20.java
@@ -1,2 +1,43 @@
 package pack1;
 
+public class PatternDemo20 {
+	public static void main(String[] args) {
+		int i,j,k;
+		for(i=1;i<=5;i++)
+		{
+			for(k=1;k<=i;k++)
+			{
+				System.out.print(" ");
+			}
+			for(j=1;j<=2*(5-i+1)-1;j++)
+			{
+				if(i==1||j==1||j==2*(5-i+1)-1)
+				{
+					System.out.print("*");
+				}
+				else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+		for(i=1;i<=5;i++)
+		{
+			for(k=1;k<=5-i+1;k++)
+			{
+				System.out.print(" ");
+			}
+			for(j=1;j<=2*i-1;j++)
+			{
+				if(i==5||j==1||j==2*i-1)
+				{
+					System.out.print("*");
+				}
+				else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+	}
+}
