3640699e56977db300b0c92d884519e51410ad11
Sanket Bhavar <88875216+SanketBhavar@users.noreply.github.com>
2022-09-27 17:56:35 +0530

    Star Pattern

--- Java/src/pack1/PatternDemo22.java
+++ Java/src/pack1/PatternDemo22.java
@@ -1,2 +1,15 @@
 package pack1;
 
+public class PatternDemo22 {
+	public static void main(String[] args) {
+		int i,j;
+		for(i=1;i<=5;i++)
+		{
+			for(j=1;j<=(i*i);j++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
