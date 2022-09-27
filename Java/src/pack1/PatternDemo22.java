c38f371417f09d134fa672a95495ba5e18ee2acb
Sanket Bhavar <88875216+SanketBhavar@users.noreply.github.com>
2022-09-27 17:56:12 +0530

    Update PatternDemo22.java

--- Java/src/pack1/PatternDemo22.java
+++ Java/src/pack1/PatternDemo22.java
@@ -1,15 +1,2 @@
 package pack1;
 
-public class PatternDemo22 {
-	public static void main(String[] args) {
-		int i,j;
-		for(i=1;i<=5;i++)
-		{
-			for(j=1;j<=(i*i);j++)
-			{
-				System.out.print("*");
-			}
-			System.out.println();
-		}
-	}
-}
