9fb2a13fef0dbf8983769847483941fdeab14bd5
Sanket Bhavar <88875216+SanketBhavar@users.noreply.github.com>
2022-09-28 18:51:49 +0530

    Update PatternDemo4.java

--- Java/src/pack1/PatternDemo4.java
+++ Java/src/pack1/PatternDemo4.java
@@ -1,15 +1,3 @@
 package pack1;
 
-public class PatternDemo4 {
-	public static void main(String[] args) {
-		int i,j;
-		for(i=1;i<=5;i++)
-		{
-			for(j=1;j<=5;j++)
-			{
-				System.out.print(j);
-			}
-			System.out.println();
-		}
-	}
-}
+
