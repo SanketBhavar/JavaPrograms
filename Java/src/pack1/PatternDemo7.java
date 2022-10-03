29bbed60d7eecb748513fdf4f7ff6aadf37bf9a4
Sanket Bhavar <88875216+SanketBhavar@users.noreply.github.com>
2022-10-03 21:06:53 +0530

    Update PatternDemo7.java

--- Java/src/pack1/PatternDemo7.java
+++ Java/src/pack1/PatternDemo7.java
@@ -2,19 +2,4 @@ package pack1;
 
 public class PatternDemo7 {
 
-	public static void main(String[] args) {
-		int i,j,k;
-		for(i=1;i<=5;i++)
-		{
-			for(j=1;j<=5;j++)
-			{
-				System.out.print(" ");
-			}
-			for(k=1;k<=5;k++)
-			{
-				System.out.print("*");
-			}
-			System.out.println();
-		}
-	}
-}
+	
