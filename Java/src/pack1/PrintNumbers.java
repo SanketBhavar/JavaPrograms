cc1ffbde7a1fba4c773d1dbf8a18ec33aa6c6820
Sanket Bhavar <88875216+SanketBhavar@users.noreply.github.com>
2022-10-13 13:07:06 +0530

    Update PrintNumbers.java

--- Java/src/pack1/PrintNumbers.java
+++ Java/src/pack1/PrintNumbers.java
@@ -1,17 +1,3 @@
 package pack1;
 
-import java.util.Scanner;
 
-public class PrintNumbers {
-	public static void main(String[] args) {
-		int n;
-	    Scanner input= new Scanner(System.in);
-	    n=input.nextInt();
-	    int counter=1;
-	    while (counter<=n){
-	      System.out.println(counter);
-	      counter=counter+1;
-	   }
-	    input.close();
-	}
-}
