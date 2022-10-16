ebcaaaf34edebcc564cb30989b7a2f19bd7ff60c
Sanket Bhavar <88875216+SanketBhavar@users.noreply.github.com>
2022-10-16 16:34:09 +0530

    Update RemainderExample.java

--- Java/src/pack1/RemainderExample.java
+++ Java/src/pack1/RemainderExample.java
@@ -2,19 +2,4 @@ package pack1;
 
 import java.util.Scanner;
 
-public class RemainderExample {
-	public static void main(String args[]) {
-	    Scanner scan = new Scanner(System.in);		
-	    int num1 = scan.nextInt();
-	    int num2 = scan.nextInt();
-		int a = num1;
-	    int b = num2;
-		while (b != 0) {
-	      int temp = b;
-	      b = a % b;
-	      a = temp;
-	    }
-	    System.out.println(a);
-	    scan.close();
-	}
-}
+
