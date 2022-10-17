246778576285e60052d28737ef8371bf90f67eeb
Sanket Bhavar <88875216+SanketBhavar@users.noreply.github.com>
2022-10-17 17:14:27 +0530

    Update RoomApp.java

--- Java/src/pack1/RoomApp.java
+++ Java/src/pack1/RoomApp.java
@@ -1,2 +1,35 @@
 package pack1;
 
+class Room{
+	int l,b;
+	Room(int x,int y){
+		l=x;
+		b=y;
+	}
+	void area() {
+		int res;
+		res=l*b;
+		System.out.println("Area is "+res);
+	}
+}
+
+class BedRoom extends Room{
+	int h;
+	BedRoom(int x,int y,int z){
+		super(x,y);
+		h=z;
+	}
+	void volume() {
+		int res;
+		res=l*b*h;
+		System.out.println("Volume is "+res);
+	}
+}
+public class RoomApp {
+	public static void main(String[] args) {
+		BedRoom b1;
+		b1 = new BedRoom(10,20,30);
+		b1.area();
+		b1.volume();
+	}
+}
