3444517594e56b65965b209c6a1912165062ad90
Sanket Bhavar <88875216+SanketBhavar@users.noreply.github.com>
2022-10-22 18:05:10 +0530

    String In-built Methods

--- Java/src/pack1/StrInbuiltMethods.java
+++ Java/src/pack1/StrInbuiltMethods.java
@@ -1,3 +1,15 @@
 package pack1;
 
-
+public class StrInbuiltMethods {
+	public static void main(String[] args) {
+		String s1="RajaRamMohanRoy";
+		System.out.println(s1.length());
+		System.out.println(s1.charAt(5));
+		System.out.println(s1.indexOf("R"));
+		System.out.println(s1.lastIndexOf("R"));
+		System.out.println(s1.toUpperCase());
+		System.out.println(s1.toLowerCase());
+		System.out.println(s1.substring(7));
+		System.out.println(s1.substring(4,10));
+	}
+}
