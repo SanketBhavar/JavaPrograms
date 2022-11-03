c0c8ff53db699cc8715cc1b3cea048001f519278
Sanket Bhavar <88875216+SanketBhavar@users.noreply.github.com>
2022-11-03 09:09:05 +0530

    String Tokenizer

--- Java/src/pack1/StringDemo.java
+++ Java/src/pack1/StringDemo.java
@@ -1,3 +1,20 @@
 package pack1;
 
+import java.util.Scanner;
+import java.util.StringTokenizer;
 
+public class StringDemo {
+	public static void main(String[] args) {
+		String s1;
+		Scanner sc = new Scanner(System.in);
+		s1=sc.next();
+		StringTokenizer stk;
+		stk=new StringTokenizer(s1,",");
+		while(stk.hasMoreTokens())
+		{
+			String token=stk.nextToken();
+			System.out.println(token);
+		}
+		sc.close();
+	}
+}
