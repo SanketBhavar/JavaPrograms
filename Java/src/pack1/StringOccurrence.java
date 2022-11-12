bb4cfd060966817d208038f47f3b60eb3fad8f20
Sanket Bhavar <88875216+SanketBhavar@users.noreply.github.com>
2022-11-12 20:52:50 +0530

    Update StringOccurrence.java

--- Java/src/pack1/StringOccurrence.java
+++ Java/src/pack1/StringOccurrence.java
@@ -1,11 +1,2 @@
 package pack1;
 
-public class StringOccurrence {
-	public static void main(String[] args) {
-		String str="Hello World";
-		int s1=str.length();
-		int s2=str.replace("l", "").length();
-		int count=s1-s2;
-		System.out.println("No. of Occurrences: "+count);
-	}
-}
