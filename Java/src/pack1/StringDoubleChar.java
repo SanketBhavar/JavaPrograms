eabbaf949d1916ac877a5f76d50a2bf3cf39850f
Sanket Bhavar <88875216+SanketBhavar@users.noreply.github.com>
2022-11-12 20:50:18 +0530

    Update StringDoubleChar.java

--- Java/src/pack1/StringDoubleChar.java
+++ Java/src/pack1/StringDoubleChar.java
@@ -1,19 +1,6 @@
 package pack1;
 
-public class StringDoubleChar {
-	public String doubleChar(String str) 
-	{
-		String s1 = "";
-		for (int i = 0; i <=str.length()-1;i++) 
-		{ 
-			s1 = s1 + str.charAt(i) + str.charAt(i);
-		}
-		return s1;
-	}
-	public static void main(String[] args) 
-	{
-		StringDoubleChar s= new StringDoubleChar();
-		String res=s.doubleChar("Sanket");
-		System.out.println(res);
-	}
-}
+
+
+
+
