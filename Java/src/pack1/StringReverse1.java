97a6d7fdcd77e0d5333c33354cf9004d8021e311
Sanket Bhavar <88875216+SanketBhavar@users.noreply.github.com>
2022-12-09 23:05:49 +0530

    Update StringReverse1.java

--- Java/src/pack1/StringReverse1.java
+++ Java/src/pack1/StringReverse1.java
@@ -6,7 +6,24 @@ public class StringReverse1 {
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
 		String str=sc.nextLine();
+		String rev="",rev1="";
+		int len=str.length();
+		//1st way
+		for(int i=len-1;i>=0;i--)
+		{
+			rev=rev+str.charAt(i);
+		}
+		System.out.println(rev);
+		System.out.println();
+		//2nd way
+		char a[]=str.toCharArray();
+		int len1=a.length;
 		
+		for(int i=len1-1;i>=0;i--)
+		{
+			rev1=rev1+a[i];
+		}
+		System.out.println(rev1);
 		sc.close();
 	}
 }
