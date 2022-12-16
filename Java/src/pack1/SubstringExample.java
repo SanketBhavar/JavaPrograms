9935fab975e2f44805765bafde93950da1f8620e
Sanket Bhavar <88875216+SanketBhavar@users.noreply.github.com>
2022-12-16 22:01:30 +0530

    Update SubstringExample.java

--- Java/src/pack1/SubstringExample.java
+++ Java/src/pack1/SubstringExample.java
@@ -1,14 +1,5 @@
 package pack1;
 
 public class SubstringExample {
-	public String makeOutWord(String out, String word)
-	{
-		return out.substring(0,2) + word + out.substring(2,4);
-	}
-	public static void main(String[] args) 
-	{
-		SubstringExample s= new SubstringExample();
-		String res=s.makeOutWord("<<>>","Java Programming");
-		System.out.println(res);
-	}
+	
 }
