e9d4a6d9ab276fb94e6744c902bd17fc30e17740
Sanket Bhavar <88875216+SanketBhavar@users.noreply.github.com>
2022-12-16 22:04:31 +0530

    Divide the string in two parts

--- Java/src/pack1/SubstringExample2.java
+++ Java/src/pack1/SubstringExample2.java
@@ -1,5 +1,14 @@
 package pack1;
 
 public class SubstringExample2 {
-	
+	public String firstHalf(String str)
+	{
+		return str.substring(0,str.length() / 2);
+	}
+	public static void main(String[] args) 
+	{
+		SubstringExample2 s= new SubstringExample2();
+		String res=s.firstHalf("Sanket");
+		System.out.println(res);
+	}
 }
