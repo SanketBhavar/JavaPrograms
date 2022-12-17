0d16cabe8e2c16d1f332fd98db37b75663f2df74
Sanket Bhavar <88875216+SanketBhavar@users.noreply.github.com>
2022-12-17 22:45:40 +0530

    Update SumOfDigit.java

--- Java/src/pack1/SumOfDigit.java
+++ Java/src/pack1/SumOfDigit.java
@@ -4,6 +4,17 @@ import java.util.Scanner;
 
 public class SumOfDigit {
 	public static void main(String[] args) {
-		
+		int n,sum,digit;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a number");
+		n=sc.nextInt();
+		sum=0;
+		while(n!=0) {
+			digit=n%10;
+			sum=sum+digit;
+			n=n/10;
+		}
+		System.out.println("Sum of digit: "+sum);
+		sc.close();
 	}
 }
