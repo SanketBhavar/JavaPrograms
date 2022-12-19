bfb94618b4772bd804e8cc36371c23bd6bf07620
Sanket Bhavar <88875216+SanketBhavar@users.noreply.github.com>
2022-12-19 21:47:05 +0530

    Update SuperKey.java

--- Java/src/pack1/SuperKey.java
+++ Java/src/pack1/SuperKey.java
@@ -1,2 +1,24 @@
 package pack1;
+class A{
+	int i;
+}
+class B extends A{
+	int i;
+	void setData(int x, int y) {
+		super.i=x;
+			  i=y;
+	}
+	void display() {
+		System.out.println("A's i is "+super.i);
+		System.out.println("B's i is "+i);
+	}
+}
+public class SuperKey {
+	public static void main(String[] args) {
+		B b1;
+		b1=new B();
+		b1.setData(10, 20);
+		b1.display();
+	}
+}
 
