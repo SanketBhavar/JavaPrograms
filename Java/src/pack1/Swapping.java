4237875727e916ed1cfb0969e26533f0a480312a
Sanket Bhavar <88875216+SanketBhavar@users.noreply.github.com>
2022-12-20 22:57:50 +0530

    Update Swapping.java

--- Java/src/pack1/Swapping.java
+++ Java/src/pack1/Swapping.java
@@ -2,6 +2,18 @@ package pack1;
 
 public class Swapping {
    public static void main(String[] args) {
-     
+      int a,b;
+      a=1;
+      b=2;
+      System.out.println("Numbers before swap");
+      System.out.println("a:"+a);
+      System.out.println("b:"+b);
+      int temp;
+      temp=a;
+      a=b;
+      b=temp;
+      System.out.println("Numbers after swap");
+      System.out.println("a="+a);
+      System.out.println("b="+b);
    }
 }
