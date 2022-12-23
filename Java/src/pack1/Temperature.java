8230cdb86b8e67c736ab78badf6fcd1e8d9d7f89
Sanket Bhavar <88875216+SanketBhavar@users.noreply.github.com>
2022-12-23 20:53:06 +0530

    Calculating Temperature C to F

--- Java/src/pack1/Temperature.java
+++ Java/src/pack1/Temperature.java
@@ -4,6 +4,12 @@ import java.util.Scanner;
 
 public class Temperature {
 	public static void main(String[] args) {
-	
+	    int C, F;
+	    Scanner input= new Scanner(System.in);
+	    System.out.println("Enter Temperature in Celcius");
+	    C=input.nextInt();
+	    F=9*C/5+32;
+	    System.out.println("Celcius to Fahrenheit: "+F);
+	    input.close();
    }
 }
