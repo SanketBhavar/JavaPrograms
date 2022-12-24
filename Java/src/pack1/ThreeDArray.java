ef3682fc76557b07bc336233001f24a81ebbae1a
Sanket Bhavar <88875216+SanketBhavar@users.noreply.github.com>
2022-12-24 22:08:38 +0530

    Update ThreeDArray.java

--- Java/src/pack1/ThreeDArray.java
+++ Java/src/pack1/ThreeDArray.java
@@ -4,6 +4,33 @@ import java.util.Scanner;
 
 public class ThreeDArray {
 	public static void main(String[] args) {
-		
+		int a[][][];
+		a=new int[3][4][5];
+		Scanner scan=new Scanner(System.in);
+		int i,j,k;
+		for(i=0;i<=a.length-1;i++)
+		{
+			for(j=0;j<=a[i].length-1;j++)
+			{
+				for(k=0;k<=a[i][j].length-1;k++)
+				{
+					System.out.println("Enter an Element");
+					a[i][j][k]=scan.nextInt();
+				}
+			}
+		}
+		for(i=0;i<=a.length-1;i++)
+		{
+			for(j=0;j<=a[i].length-1;j++)
+			{
+				for(k=0;k<=a[i][j].length-1;k++)
+				{
+					System.out.print(a[i][j][k]+" ");
+				}
+				System.out.println();
+			}
+			System.out.println();
+		}
+		scan.close();
 	}
 }
