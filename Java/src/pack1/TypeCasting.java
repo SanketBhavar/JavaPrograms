5fe74371224afa5b31cf5aedf2633b72658c4112
Sanket Bhavar <88875216+SanketBhavar@users.noreply.github.com>
2022-12-25 22:05:32 +0530

    Update TypeCasting.java

--- Java/src/pack1/TypeCasting.java
+++ Java/src/pack1/TypeCasting.java
@@ -5,16 +5,17 @@ import java.util.Scanner;
 public class TypeCasting {
 	public static void main(String[] args) {
 		Scanner input= new Scanner(System.in);
-	    int a=input.nextInt();
-	    float f=input.nextFloat();
-	    double d=input.nextDouble();
-	    float x;
-	    int y;
-	    float z;
-	    x=(float)a;
-	    y=(int)f;
-	    z=(float)d;
-	    System.out.println("x="+x+" y="+y+" z="+z);
+	    	int a=input.nextInt();
+	    	float f=input.nextFloat();
+	    	double d=input.nextDouble();
+	    	float x;
+	    	int y;
+	    	float z;
+		//Explicit Type Casting
+	    	x=(float)a;
+	    	y=(int)f;
+	    	z=(float)d;
+	    	System.out.println("x="+x+" y="+y+" z="+z);
 	    input.close();
 	}
 }
